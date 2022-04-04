43445a9ecc7e08b6253e1aea06980d393bf5904f
Jonathan Gulliksen <jonathangullik@gmail.com>
2022-04-04 20:17:09 +0200

    made route to shop

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import { Navbar } from "./pages/Navbar";
 import { Route, BrowserRouter as Router, Link, Routes } from "react-router-dom";
 import React from "react";
 import { ErrorPage } from "./pages/ErrorPage";
+import { ShopPage } from "./pages/comp/ShopPage";
 
 function App() {
   return (
     <Router>
-      <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="portfolio" element={<ProjectComponent />} />
+        <Route path="shop" element={<ShopPage />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
