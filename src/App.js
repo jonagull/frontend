b23c43e67c073789da39db3b01e371d193deb802
Jonathan Gulliksen <jonathangullik@gmail.com>
2022-04-04 19:15:15 +0200

    added routing app

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import "./App.scss";
 import { ProjectComponent } from "./pages/comp/ProjectComponent";
 import { LandingPage } from "./pages/LandingPage";
 import { Navbar } from "./pages/Navbar";
+import { Route, BrowserRouter as Router, Link, Routes } from "react-router-dom";
+import React from "react";
+import { ErrorPage } from "./pages/ErrorPage";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-      <LandingPage />
-      <ProjectComponent />
-    </div>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="portfolio" element={<ProjectComponent />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
   );
 }
 
