1db86eec0be3eb061801ac47dcc4c6b11b8bfaee
Jonathan Gulliksen <jonathangullik@gmail.com>
2022-04-04 20:37:13 +0200

    init mic and mixer pages

---
+++ src/pages/comp/ShopPage/shopCategoriesPages/mics/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const MicsPage = () => {
+  return (
+    <div className="mics__wrapper">
+      <div className="title__container">
+        <h1>MICS</h1>
+      </div>
+    </div>
+  );
+};
---
+++ src/pages/comp/ShopPage/shopCategoriesPages/mixers/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const MixersPage = () => {
+  return (
+    <div className="mixers__wrapper">
+      <h1>MIXERS</h1>
+    </div>
+  );
+};
