fb208be23a3f9cd9c3747d7193f0d500da0dc4c7
Jonathan Gulliksen <jonathangullik@gmail.com>
2022-04-04 20:37:00 +0200

    added routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { Route, BrowserRouter as Router, Link, Routes } from "react-router-dom";
 import React from "react";
 import { ErrorPage } from "./pages/ErrorPage";
 import { ShopPage } from "./pages/comp/ShopPage";
+import { MicsPage } from "./pages/comp/ShopPage/shopCategoriesPages/mics";
+import { MixersPage } from "./pages/comp/ShopPage/shopCategoriesPages/mixers";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="portfolio" element={<ProjectComponent />} />
         <Route path="shop" element={<ShopPage />} />
+        <Route path="/shop/mics" element={<MicsPage />} />
+        <Route path="/shop/mixers" element={<MixersPage />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
