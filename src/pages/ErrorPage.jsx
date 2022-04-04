4aaf8b2ed145308eae40a58e5cb9d084f43cde13
Jonathan Gulliksen <jonathangullik@gmail.com>
2022-04-04 20:36:49 +0200

    added home navigation

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,10 +1,13 @@
+import { useNavigate } from "react-router-dom";
 import { Navbar } from "./Navbar";
 
 export const ErrorPage = () => {
+  let navigate = useNavigate();
+
   return (
     <div>
-      <Navbar />
       <h1>Error page go back!</h1>
+      <button onClick={() => navigate("/")}>Home</button>
     </div>
   );
 };
