c301ad896be6db6eec3ea4e2ddb6a71e4f4c5283
Jonathan Gulliksen <jonathangullik@gmail.com>
2022-07-08 14:19:32 +0200

    Changes to dukPage

--- src/pages/ShopPage/DukPage/ServicesContainer.jsx
+++ src/pages/ShopPage/DukPage/ServicesContainer.jsx
@@ -14,7 +14,6 @@ const ServivceContainer = styledComponents.div`
     margin: 40px;
     padding: 50px;
     border-radius: 30px;
-    box-shadow: 4px 8px 36px -13px rgba(0,0,0,0.75);
 `;
 
 export function ServicesContainer({ data }) {
