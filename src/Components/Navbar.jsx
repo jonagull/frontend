4263b41809ce1a7c86347375ba1f6e41af8c55e6
Jonathan Gulliksen <jonathangullik@gmail.com>
2022-07-09 17:45:56 +0200

    styling navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ScrollIntoView from "react-scroll-into-view";
+import HardwareIcon from "@mui/icons-material/Hardware";
+import HomeRepairServiceIcon from "@mui/icons-material/HomeRepairService";
 
 export const Navbar = ({ invert }) => {
   let navigate = useNavigate();
@@ -25,7 +27,14 @@ export const Navbar = ({ invert }) => {
             : "navbar__wrapper navbar__bg"
         }
       >
-        <div style={{ display: "flex" }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            width: "100%",
+            padding: "0 60px",
+          }}
+        >
           <h1
             onClick={() => {
               navigate("/");
@@ -33,21 +42,39 @@ export const Navbar = ({ invert }) => {
           >
             DUK
           </h1>
-          {invert && (
-            <ScrollIntoView alignToTop={true} selector="#hr-arbeid">
-              <h1 style={{ marginLeft: "200px" }}>Arbeid</h1>
-            </ScrollIntoView>
-          )}
-          {invert && (
-            <ScrollIntoView selector=".title__container">
-              <h1 style={{ marginLeft: "200px" }}>Utleie</h1>
-            </ScrollIntoView>
-          )}
-          {!invert && (
-            <ScrollIntoView alignToTop={true} selector="#hr-prosjekter">
-              <h1 style={{ marginLeft: "200px" }}>Prosjekter</h1>
-            </ScrollIntoView>
-          )}
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-around",
+              alignItems: "center",
+              width: "40%",
+            }}
+          >
+            {invert && (
+              <ScrollIntoView alignToTop={true} selector="#hr-arbeid">
+                <span>
+                  <HardwareIcon />
+                  <h2>Arbeid</h2>
+                </span>
+              </ScrollIntoView>
+            )}
+            {invert && (
+              <ScrollIntoView selector=".title__container">
+                <span>
+                  <HomeRepairServiceIcon />
+                  <h2>Utleie</h2>
+                </span>
+              </ScrollIntoView>
+            )}
+            {!invert && (
+              <ScrollIntoView alignToTop={true} selector="#hr-prosjekter">
+                <span>
+                  <HardwareIcon />
+                  <h2>Prosjekter</h2>
+                </span>
+              </ScrollIntoView>
+            )}
+          </div>
         </div>
       </div>
     </React.Fragment>
