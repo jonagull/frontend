b5695430016df3951869725d312072af4da113c3
jonagull <jonathangullik@gmail.com>
2022-08-29 18:26:14 +0200

    button fix

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -26,7 +26,7 @@ export const LandingPage = () => {
           <h4>Om Daniel</h4>
           <h4 style={{ position: "absolute", top: "25%" }}>Utvalgte Prosjekter</h4>
           <Button variant="contained" className="button"
-            style={{ backgroundColor: "white", color: "black", borderRadius: "8px" }} onClick={() => {
+            style={{ backgroundColor: "white", color: "black", borderRadius: "8px", fontWeight: "700", fontFamily: "poppins", fontSize: "20px" }} onClick={() => {
               navigate("/portfolio");
             }}>Daniel</Button>
         </div>
@@ -36,7 +36,7 @@ export const LandingPage = () => {
           <h4 style={{ color: "black", position: "absolute", top: "25%" }}>Tidligere Arbeid</h4>
           <h4 style={{ color: "black", position: "absolute", top: "30%" }}>Utleie</h4>
           <Button className="button"
-            style={{ backgroundColor: "black", color: "white", borderRadius: "8px" }}
+            style={{ backgroundColor: "black", color: "white", borderRadius: "8px", fontWeight: "700", fontFamily: "poppins", fontSize: "20px" }}
             onClick={() => {
               navigate("/shop");
             }}
